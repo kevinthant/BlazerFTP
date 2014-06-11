82c07027e6b11a6db13b412899fe27237469b9d9
Kevin Thant <kevinthant.nyc@gmail.com>
2014-06-11 17:20:08 -0400

    Saving work in progress for completing synchronizatin feature

---
+++ main.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'BlazerFTP.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+$time_start = microtime(true);
+$output = BlazerFTP::buildHash('C:\Projects\fidelitynfs');
+$time_end = microtime(true);
+//dividing with 60 will give the execution time in minutes other wise seconds
+$execution_time = ($time_end - $time_start);
+
+//execution time of the script
+echo '<b>Total Execution Time:</b> '.number_format($execution_time).' seconds <br/>';
+
+print_r($output); */
+
+
+$ftp = new BlazerFTP('93.188.160.80', 'u516621118', 'blazer123');
+
+//$ftp->sync('C:\wamp\www\dummyA', null, 'C:\wamp\www\dummyB');
+$ftp->sync('C:\Users\kthant\Documents\GitHub\lawfirm', '/public_html');
